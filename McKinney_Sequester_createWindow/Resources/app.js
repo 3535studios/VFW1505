ae4dd2543df3602196859340f2addb4c6aeea2af
Sequester McKinney <quest@3535studios.com>
2015-05-07 20:51:17 -0400

    Create a view.

---
+++ McKinney_Sequester_createWindow/Resources/app.js
@@ -0,0 +1,20 @@
+Ti.UI.setBackgroundColor("#000");
+
+//create a window
+var mainWindow = Ti.UI.createWindow({
+	//Reserved Properties
+	backgroundColor: "#fff",
+	backgroundImage: "KS_nav_ui.png",
+	backgroundRepeat: true
+});
+
+var myFirstView = Ti.UI.createView({
+	backgroundColor: "#333",
+	borderRadius: 5,
+	borderWidth: 1,
+	width: 100,
+	height: 100
+});
+
+mainWindow.add(myFirstView);
+mainWindow.open();
