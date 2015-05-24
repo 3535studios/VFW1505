71f812c9addfc7066be09b142f85ee3b2f528e3e
Sequester McKinney <quest@3535studios.com>
2015-05-23 23:46:14 -0400

    Complete with the exception of title for image.

--- McKinney_Sequester_Project3/Resources/gallery.js
+++ McKinney_Sequester_Project3/Resources/gallery.js
@@ -8,6 +8,31 @@ var margin = 5;
 var trueCanvasWidth = pWidth - (rowCount * (margin * 2));
 var size =  trueCanvasWidth / rowCount;
 
+//GET GALLERY DETAIL
+var getGalleryDetail = function(dataSource) {
+	var galleryDetailWindow = Ti.UI.createWindow ({
+		backgroundColor: "#fff",
+		title: "Detail"
+	});
+	for (var i=0; i<imageFiles.length; i++) {
+		var imageView = Ti.UI.createView ({
+			backgroundColor: "#042F56",
+		});
+	
+		var fullSizeImage = Ti.UI.createImageView ({
+		  image: dataSource,
+		  title.dataSource.text
+		});
+	
+}
+	
+	imageView.add(fullSizeImage);
+	galleryDetailWindow.add(imageView);
+	navWindow.openWindow(galleryDetailWindow);
+};
+
+
+
 //OPEN FULL GALLERY WINDOW
 var getGallery = function() {
 	var galleryWindow = Ti.UI.createWindow ({
@@ -38,12 +63,18 @@ for (var i=0; i<imageFiles.length; i++) {
 	});
 	
 	var thumb = Ti.UI.createImageView ({
-		image: "images/gallery/" + imageFiles[i],
+		image: "images/gallery/" + imageFiles[i]
 	});
 	
 	view.add(thumb);
 	viewContainer.add(view);
+	//view.addEventListener("click", getGalleryDetail);
 }	
+
+//EVENT PROGATION
+viewContainer.addEventListener("click", function(event){
+	getGalleryDetail(event.source.image);
+});
 
 galleryWindow.add(viewContainer);
 navWindow.openWindow(galleryWindow);
