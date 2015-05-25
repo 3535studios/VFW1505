3db756cf8d2700931cf177e4a7d7bf4f2040ace0
Sequester McKinney <quest@3535studios.com>
2015-05-25 11:42:42 -0400

    Setting up labels.

--- McKinney_Sequester_Project4/Resources/app.js
+++ McKinney_Sequester_Project4/Resources/app.js
@@ -2,12 +2,27 @@
 var mainWindow = Ti.UI.createWindow ({
 	backgroundColor: "#401515",
 	title: "US NAVY SUBMARINES",
-	fontFamily: "ProximaNova-Semibold",
-	fontSize: "40px"
+	fontFamily: "OpenSans-Regular",
+	fontSize: 40
+});
+
+var galleryView = Ti.UI.createView ({
+	backgroundColor: "1c3d59",
+	height: "129px",
+	top: 60
+});
+
+var galleryLabel = Ti.UI.createLabel ({
+	text: "GALLERY",
+	fontFamily: "OpenSans-Semibold",
+	fontSize: 80,
+	color: "#fff"
 });
 
 var navWindow = Ti.UI.iOS.createNavigationWindow ({
 	window: mainWindow,
 });
 
+galleryView.add(galleryLabel);
+navWindow.add(galleryView);
 navWindow.open();
